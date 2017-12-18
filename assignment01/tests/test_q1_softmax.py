768a2efe3d52c775c14aeaf094ed37b9e194c5ae
Yi Zhang <beingzy@users.noreply.github.com>
2017-12-17 21:53:23 -0800

    add test method placeholder for per_row_softmax

--- assignment01/tests/test_q1_softmax.py
+++ assignment01/tests/test_q1_softmax.py
@@ -9,10 +9,13 @@ class TestSoftmax(unittests.TestCase):
     def __init__(self):
         super(self).__init__()
 
-    def testsoftmax_func(self, x):
+    def test_softmax(self):
         x = [1, 2, 3]
         softmax_x = [0.0900305,  0.244728, 0.6652409]
         self.assertListEqual(softmax(x), softmax_x)
+
+    def test_per_row_softmax(self):
+        raise NotImplementError
 
 
 if __main__ == "__main__":
